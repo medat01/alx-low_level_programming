61f4b7ce3fb236bbc751044b432ea4d0519b2e4d
medat01 <you@example.com>
2023-09-18 16:25:40 -0700

    l

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,27 @@
-#include "main.h"
-/*dicription*/
-void reset_to_98(int *n)
+#include <stdio.h>
+
+/**
+ * updateValue - Updates the value pointed to by an integer pointer to 98.
+ * @ptr: Pointer to an integer.
+ *
+ * Description: This function takes a pointer to an integer as a parameter
+ * and updates the value it points to 98.
+ */
+void updateValue(int *ptr)
 {
-	*n = 98;
+    *ptr = 98;
 }
+
+int main()
+{
+    int num = 42;
+    printf("Before update: num = %d\n", num);
+
+    // Call the updateValue function with a pointer to num
+    updateValue(&num);
+
+    printf("After update: num = %d\n", num);
+
+    return (0);
+}
+
